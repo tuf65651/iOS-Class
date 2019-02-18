d63543bf2e49a4dfdcb99fbe226de40ad4ed2c0b
Shmuel Jacobs <tuf65651@tech-a010.tu.temple.edu>
2019-02-18 11:50:36 -0500

    Implemented random number

--- PicoFermiBagel/PicoFermiBagel/Bagels.swift
+++ PicoFermiBagel/PicoFermiBagel/Bagels.swift
@@ -21,8 +21,9 @@ class Bagels {
     
     // Return secret number as three digit string
     func generateSecretNumber() {
-        //TODO: logic for return secret number
-        secretNumber = "500";
+        // generate number in [100, 999]
+        let generated: UInt32 = arc4random_uniform(900) + 100;
+        secretNumber = String(generated);
     }
     
     // Check if arguent matches secret number
