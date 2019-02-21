5d3a5f08bf3bb38cc47c9e4587302435a635e5fc
Shmuel Jacobs <tuf65651@temple.edu>
2019-02-21 12:08:52 -0500

    Finished Lab2 Part1

--- TempConverter.playground/Contents.swift
+++ TempConverter.playground/Contents.swift
@@ -7,15 +7,25 @@ class TempConverter {
         self.temp = temp;
     }
     
-    func convert(unit: String) -> Int {
-        if unit == "F" {
-            return 5 * (self.temp - 32)/9;
-        } else {
-            return (9 * self.temp)/5 + 32;
+    static func convert(temp: Int, unit: String = "F") -> Int {
+        
+        if tempBelowAbsoluteZero(temp: temp, unit: unit){
+            return -1000;
         }
+        if (unit == "F") {
+            return 5 * (temp - 32)/9;
+        } else {
+            return (9 * temp)/5 + 32;
+        }
+    }
+    
+    static func tempBelowAbsoluteZero(temp: Int, unit: String) -> Bool {
+        return (temp < -454 && unit == "F") || (temp < -270 && unit == "C");
     }
 }
 
 let t = TempConverter(temp: 10);
 
-print(t.convert(unit: "F"));
+print(TempConverter.convert(temp: 50));
+print(TempConverter.convert(temp: 10, unit: "C"))
+print(TempConverter.convert(temp: -460, unit: "F"))
