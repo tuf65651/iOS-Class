d66e8dfd3cfbf23fc482992596325ca0e05be904
student <tuf65651@tech-a005.tu.temple.edu>
2019-03-17 22:56:46 -0400

    Quiz 1 up to end of step 9.

---
+++ TempConverterApp/TempConverterApp/AboutViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AboutViewController.swift
+//  TempConverterApp
+//
+//  Created by Shmuel Jacobs on 3/17/19.
+//  Copyright © 2019 Icebreaker Industries. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+}
