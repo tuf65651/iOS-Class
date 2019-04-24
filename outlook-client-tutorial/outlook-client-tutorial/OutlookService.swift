24e5a0735e7b0a24adbdc27d5142f115c5f15942
Shmuel Jacobs <tuf65651@cst11220.tu.temple.edu>
2019-04-24 11:01:14 -0400

    Fixed my mistake in the oauth url. Began Mail Client

--- outlook-client-tutorial/outlook-client-tutorial/OutlookService.swift
+++ outlook-client-tutorial/outlook-client-tutorial/OutlookService.swift
@@ -8,12 +8,15 @@
 
 import Foundation
 import p2_OAuth2
+import SwiftyJSON
 
 class OutlookService {
     
+    private var userEmail: String;
+    
     private static let oauth2Settings = [
         "client_id" : "cc40fc6c-ce8a-414c-b7d1-cf616310f7be",
-        "authorize_uri": "https://login.microsoftonline.com/common/oauth2/v2.0/token",
+        "authorize_uri": "https://login.microsoftonline.com/common/oauth2/v2.0/authorize",
         "token_uri": "https://login.microsoftonline.com/common/oauth2/v2.0/token",
         "scope": "openid profile offline_access User.Read Mail.Read",
         "redirect_uris": ["outlook-client-tutorial://oauth2/callback"],
@@ -30,6 +33,7 @@ class OutlookService {
     private init() {
         oauth2 = OAuth2CodeGrant(settings: OutlookService.oauth2Settings);
         oauth2.authConfig.authorizeEmbedded = true;
+        userEmail = "";
     }
     
     class func shared() -> OutlookService {
@@ -64,4 +68,26 @@ class OutlookService {
         oauth2.forgetTokens();
     }
     
+    func makeApiCall(api: String, params: [String: String]? = nil, callback: @escaping (JSON?) -> Void) -> Void {
+        
+        // Build the request URL
+        var urlBuilder = URLComponents(string: "https://graph.microsoft.com")!
+        urlBuilder.path = api;
+        
+        if let unwrappedParams = params {
+            // Add query parameters to URL
+            urlBuilder.queryItems = [URLQueryItem]();
+            for (paramName, paramValue) in unwrappedParams {
+                urlBuilder.queryItems?.append(
+                    URLQueryItem(name: paramName, value: paramValue)
+                );
+            }
+        }
+        
+        let apiUrl = urlBuilder.url!;
+        NSLog("Making request to \(apiUrl)");
+        
+        var req = oauth2.request(forURL: apiUrl);
+        req.addValue("application/json", forHTTPHeaderField: "Accept");
+    }
 }
