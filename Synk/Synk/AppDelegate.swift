4b7ae01671909e205e4843aa0f7c56e27f9e9e63
Shmuel Jacobs <tuf65651@tech-a010.local>
2019-05-01 13:35:15 -0400

    Outlook Client Sample Code

--- Synk/Synk/AppDelegate.swift
+++ Synk/Synk/AppDelegate.swift
@@ -18,6 +18,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         return true
     }
+    
+    func application(_ app: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+        if url.scheme == "synk" {
+            let service = OutlookService.shared()
+            service.handleOAuthCallback(url: url)
+            return true
+        }
+        else {
+            return false
+        }
+    }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
